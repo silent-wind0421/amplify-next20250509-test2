2a0488e6e44293815c93cebe5b4084a700043ba8
吉田 明弘 <yoshida@makotoya.tech>
2025-05-30 16:34:09 +0900

    modified20250530cc

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,8 +9,8 @@ specifies that any user authenticated via an API key can "create", "read",
 const schema = a.schema({
   Login: a
     .model({
-      uid: a.string().required(),
-      loginTime: a.datetime().required(),
+      uid: a.string(),
+      loginTime: a.datetime(),
 
     })
 
